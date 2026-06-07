a68942c24c794e1edb0a4fb8676ba6ec34d717ae
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-07 14:50:10 +0200

    Add links to open

--- tasks/run_windows_dev_application.ps1
+++ tasks/run_windows_dev_application.ps1
@@ -1,2 +1,5 @@
 Start-Process uv -ArgumentList "run", "python", "src\main.py" ; 
 Start-Process "http://localhost:8001" ; 
+Start-Process "http://localhost:8001/openapi.json" ; 
+Start-Process "http://localhost:8001/redoc" ; 
+Start-Process "http://localhost:8001/docs" ; 
--- tasks/tilt.ps1
+++ tasks/tilt.ps1
@@ -1,4 +1,7 @@
 Start-Process "http://localhost:10350" ; 
 Start-Process "http://localhost:8003" ; 
+Start-Process "http://localhost:8003/openapi.json" ; 
+Start-Process "http://localhost:8003/redoc" ; 
+Start-Process "http://localhost:8003/docs" ; 
 
 .\scripts\tilt-up.ps1 ; 
