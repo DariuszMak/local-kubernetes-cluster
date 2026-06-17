ac3a971d82d204d273662c2f7c1567c2faabeca3
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 12:56:46 +0200

    update commands for almebic

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,5 +1,10 @@
 .\scripts\format_and_lint.ps1 ; 
 
-uv run pytest tests/ --cov=src -vv ; 
+uv run pytest tests/ tests2/ --cov=src --cov=src2 -vv ; 
 
-uv run alembic upgrade head ; uv run alembic downgrade base ; uv run alembic upgrade head ; 
+uv run alembic upgrade head ; 
+uv run alembic -c alembic2.ini upgrade head ; 
+uv run alembic downgrade base ; 
+uv run alembic -c alembic2.ini downgrade base ; 
+uv run alembic upgrade head ; 
+uv run alembic -c alembic2.ini upgrade head ; 
