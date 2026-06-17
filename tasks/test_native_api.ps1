d2e95fae4f324bf285322483b5577f0bff00dccc
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 13:03:53 +0200

    update ps newman scripts

--- tasks/test_native_api.ps1
+++ tasks/test_native_api.ps1
@@ -1,1 +1,2 @@
 newman run collections\API.postman_collection.json --environment collections\environments\API_native.postman_environment.json ; 
+newman run collections\API.postman_collection.json --environment collections\environments\API2_native.postman_environment.json ; 
--- tasks/test_tilt_api.ps1
+++ tasks/test_tilt_api.ps1
@@ -1,1 +1,2 @@
 newman run collections\API.postman_collection.json --environment collections\environments\API_tilt.postman_environment.json ; 
+newman run collections\API.postman_collection.json --environment collections\environments\API2_tilt.postman_environment.json ; 
