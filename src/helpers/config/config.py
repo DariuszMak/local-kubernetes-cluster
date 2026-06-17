401624bbad15d0ab597068b3ad6440596a7dc21e
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 22:28:48 +0200

    unify config file for src

--- src/helpers/config/config.py
+++ src/helpers/config/config.py
@@ -3,21 +3,21 @@ from functools import lru_cache
 
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from src2.helpers.config.env_loader_mixin import EnvLoaderMixin
+from src.helpers.config.env_loader_mixin import EnvLoaderMixin
 
 
 @dataclass(frozen=True)
 class Config(EnvLoaderMixin):
-    log_file: str = "app2.log"
+    log_file: str = "app.log"
 
 
 class Settings(BaseSettings):
-    app_name: str = "App2 API"
+    app_name: str = "App API"
     debug: bool = True
     version: str = "1.0.0"
-    database_url: str = "sqlite+aiosqlite:///./app2.db"
-    host: str = Field(default="127.0.0.1", validation_alias="HOST2")
-    port: int = Field(default=8002, validation_alias="PORT2")
+    database_url: str = "sqlite+aiosqlite:///./app.db"
+    host: str = Field(default="127.0.0.1", validation_alias="HOST")
+    port: int = Field(default=8001, validation_alias="PORT")
 
     model_config = SettingsConfigDict(
         env_file=".env",
