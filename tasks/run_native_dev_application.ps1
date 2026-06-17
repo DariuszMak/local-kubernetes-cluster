01e64aad3b67117721395a1ecd2d83f45920ea5f
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 12:50:34 +0200

    modify native dev application

--- tasks/run_native_dev_application.ps1
+++ tasks/run_native_dev_application.ps1
@@ -1,5 +1,12 @@
 Start-Process uv -ArgumentList "run", "python", "src\main.py" ; 
+Start-Process uv -ArgumentList "run", "python", "src2\main.py" ; 
+
 Start-Process "http://localhost:8001" ; 
 Start-Process "http://localhost:8001/openapi.json" ; 
 Start-Process "http://localhost:8001/redoc" ; 
 Start-Process "http://localhost:8001/docs" ; 
+
+Start-Process "http://localhost:8002" ; 
+Start-Process "http://localhost:8002/openapi.json" ; 
+Start-Process "http://localhost:8002/redoc" ; 
+Start-Process "http://localhost:8002/docs" ; 
