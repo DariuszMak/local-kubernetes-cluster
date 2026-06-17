36cb12e5b92ef87dab0e7efa63d740303a146d6a
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 15:17:45 +0200

    fix errors on imports

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,6 +1,6 @@
 .\scripts\format_and_lint.ps1 ; 
 
-uv run pytest tests/ tests2/ --cov=src --cov=src2 -vv ; 
+uv run pytest tests/ tests2/ --cov=src --cov=src2 -vv --import-mode=importlib; 
 
 uv run alembic upgrade head ; 
 uv run alembic -c alembic2.ini upgrade head ; 
