6b9e82cbbb989655f5bf07f1499df0ac677930fb
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 12:58:36 +0200

    add changes to lit script

--- scripts/format_and_lint.ps1
+++ scripts/format_and_lint.ps1
@@ -14,3 +14,22 @@ uv run mypy --strict alembic src tests
 # uv run mypy --explicit-package-bases alembic src tests
 # uv run mypy --explicit-package-bases --check-untyped-defs alembic src tests
 # uv run mypy --strict alembic src tests
+
+
+
+uv run ruff format alembic2 src2 tests2
+uv run ruff check --fix alembic2 src2 tests2
+uv run ruff check --fix --unsafe-fixes alembic2 src2 tests2
+uv run ruff check --fix --select I alembic2 src2 tests2
+
+uv run pip-audit
+uv run ruff check alembic2 src2 tests2
+uv run ruff format --check alembic2 src2 tests2
+
+uv run vulture src2 tests2 --min-confidence 80
+
+uv run mypy --strict alembic2 src2 tests2
+
+# uv run mypy --explicit-package-bases alembic2 src2 tests2
+# uv run mypy --explicit-package-bases --check-untyped-defs alembic2 src2 tests2
+# uv run mypy --strict alembic2 src2 tests2
