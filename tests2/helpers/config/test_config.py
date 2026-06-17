405f8424996a58fce66e1a6db03ddeadbc07cfea
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 17:41:43 +0200

    adjust the test

--- tests2/helpers/config/test_config.py
+++ tests2/helpers/config/test_config.py
@@ -4,4 +4,4 @@ from src2.helpers.config.config import Config
 def test_config_defaults() -> None:
     config = Config()
 
-    assert config.log_file == "app.log"
+    assert config.log_file == "app2.log"
