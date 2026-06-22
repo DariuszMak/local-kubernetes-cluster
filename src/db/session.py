9e79686c3afb7faa00cba8125fd8499bbe69676a
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-22 15:34:33 +0200

    fix session file

--- src/db/session.py
+++ src/db/session.py
@@ -1,24 +1,33 @@
-from collections.abc import AsyncGenerator
+import os
+from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from src.helpers.config.config import get_settings
 
-settings = get_settings()
+
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://postgres:postgres@localhost:5432/app"
+)
+
+
 
 engine = create_async_engine(
-    settings.database_url,
-    echo=settings.debug,
-    connect_args={"check_same_thread": False},
+    DATABASE_URL,
+    echo=False,  
 )
 
-AsyncSessionLocal = async_sessionmaker(
+
+async_session_maker = async_sessionmaker(
     bind=engine,
-    expire_on_commit=False,
     class_=AsyncSession,
+    expire_on_commit=False,
 )
 
 
-async def get_db() -> AsyncGenerator[AsyncSession]:
-    async with AsyncSessionLocal() as session:
-        yield session  # noqa: ASYNC119
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+
+    async with async_session_maker() as session:
+        yield session
