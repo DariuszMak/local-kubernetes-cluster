b73cf06f59a6a35c573c464f274153bc99a35571
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-22 14:35:21 +0200

    fix the tests issue

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,6 +1,7 @@
 .\scripts\format_and_lint.ps1 ; 
 
-uv run pytest tests/ tests2/ --cov=src --cov=src2 -vv --import-mode=importlib; 
+uv run pytest tests/ --cov=src -vv ; 
+uv run pytest tests2/ --cov=src2 -vv ; 
 
 uv run alembic upgrade head ; 
 uv run alembic downgrade base ; 
