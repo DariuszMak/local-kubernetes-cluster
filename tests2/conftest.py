1cf45acc70294c1a91c34a15285d45e2a80b5250
Dariusz <darekmakarewicz1994@gmail.com>
2026-09-10 09:02:27 +0200

    Revert "update throttling"
    This reverts commit 335f89644cbd02b1844579a3b6e1d2a354c6df06.

--- tests2/conftest.py
+++ tests2/conftest.py
@@ -14,12 +14,6 @@ def reset_db(db: None) -> None:
 
 @pytest.fixture(autouse=True)
 def disable_throttling(settings):
-
-    settings.REST_FRAMEWORK = {
-        **settings.REST_FRAMEWORK,
-        "DEFAULT_THROTTLE_CLASSES": [],
-    }
-
     settings.REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
         "anon": None,
         "user": None,
